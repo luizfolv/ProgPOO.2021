da7b7f831b175f0898b6c1f034c0c4b00f3e1e2f
Luiz Felipe Leitão <luiz.leitao@soulasalle.com.br>
2021-08-13 19:13:49 -0300

    Adicionado primeiro exercicio da primeira semana, Tabuada em java.

---
+++ Semana_01/tabuada.java
@@ -0,0 +1,13 @@
+/*
+    Matricula: 0050017010
+    Luiz Felipe Oliveira Gonçalves Leitão
+ */
+
+ public class tabuada {
+ 
+     public static void main(String[] args) {
+         for (int i = 0; i <= 10; i++) {
+             System.out.println(i + " x 10 = "+ (i*10));
+         }
+     }
+ }
